56d95e64ed40a795bc40af22b200bdd10e682ee5
piratejas <piratejas@gmail.com>
2023-05-22 09:32:35 +0100

    set upstream

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,19 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/juggling-balls-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'how-many-balls'
+    var howManyBalls = req.session.data['how-many-balls']
+  
+    // Check whether the variable matches a condition
+    if (howManyBalls == "3 or more"){
+      // Send user to next page
+      res.redirect('/juggling-trick')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/ineligible')
+    }
+  
+  })
